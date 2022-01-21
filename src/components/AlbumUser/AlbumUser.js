68c7280f84c0f0c84746e3a320b82192742181e6
Nazar-Sergeev <privat.nazar@gmail.com>
2022-01-21 18:47:56 +0200

    lesson3_hw

--- src/components/AlbumUser/AlbumUser.js
+++ src/components/AlbumUser/AlbumUser.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const AlbumUser = ({album:{userId, id, title}}) => {
     return (
         <div>
             <h4>UserId: {userId} - id: {id}</h4>
             <h5>Title: {title}</h5>
+            <Link to={`${id}/photos`}>
+                <button>Photos</button></Link>
         </div>
     );
 };
